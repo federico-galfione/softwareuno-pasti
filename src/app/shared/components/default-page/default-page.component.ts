3be6e3caa444e5b0950cbae353651dc4ed953059
Federico Galfione <federico.galfione@outlook.com>
2021-03-18 19:47:59 +0100

    Change in admin style, added roles icons

--- src/app/shared/components/default-page/default-page.component.ts
+++ src/app/shared/components/default-page/default-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ApplicationRef, Component, ElementRef, Injector, Input, OnChanges, OnInit } from '@angular/core';
 import { Roles } from '../../services/auth.service';
 
 @Component({
@@ -6,17 +6,26 @@ import { Roles } from '../../services/auth.service';
   templateUrl: './default-page.component.html',
   styleUrls: ['./default-page.component.scss'],
 })
-export class DefaultPageComponent implements OnInit {
+export class DefaultPageComponent implements OnChanges {
   @Input()
-  color: 'primary' | 'secondary' | 'tertiary';
+  color: 'primary' | 'secondary' | 'tertiary' = 'primary';
   @Input()
   title: string;
   @Input()
   subtitle: string;
   @Input()
+  sectionTitle: string;
+  @Input()
   showCountdown: boolean = true;
-  constructor() { }
+  private currentPrimaryColor: string;
+  private currentSecondaryColor: string;
 
-  ngOnInit() {}
+  constructor() { 
+  }
+
+  ngOnChanges(){
+    this.currentPrimaryColor = getComputedStyle(document.documentElement).getPropertyValue('--ion-color-'+this.color);
+    this.currentSecondaryColor = getComputedStyle(document.documentElement).getPropertyValue('--ion-color-'+this.color+'-tint');
+  }
 
 }
